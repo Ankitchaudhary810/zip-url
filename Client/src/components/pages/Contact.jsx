75736110c0daecbb4f548c0b24ed024a5a8fdf30
Ankit Chaudhary <ankitcha810@gmail.com>
2024-02-04 20:51:35 +0530

    about page final done

--- Client/src/components/pages/Contact.jsx
+++ Client/src/components/pages/Contact.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import React from "react";
 
 const Contact = () => {
   return (
-    <div>Contact</div>
-  )
-}
+    <div className="container mt-5">
+      <h3 className="mx-3">Get In Touch....</h3>
+      <form className="mx-3 my-3 w-50">
+        <div class="form-row">
+          <div class="col my-4">
+            <input type="text" class="form-control" placeholder="Name" />
+          </div>
+          <div class="col my-4">
+            <input type="text" class="form-control" placeholder="Last name" />
+          </div>
+        </div>
+      </form>
+    </div>
+  );
+};
 
-export default Contact
+export default Contact;
